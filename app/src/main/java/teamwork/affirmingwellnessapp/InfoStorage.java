52a64bc098343705a1a95a1611263fb6895ab7bd
StrawberryNougat <eleanormather02@gmail.com>
2020-10-04 21:43:57 -0400

    DataStorage
    Takes user input, stores it in parameters

---
+++ app/src/main/java/teamwork/affirmingwellnessapp/InfoStorage.java
@@ -0,0 +1,52 @@
+package teamwork.affirmingwellnessapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InfoStorage extends AppCompatActivity{
+
+    String name, pronouns, meal; //I think that we may want to add a pronoun option if the app ever uses them
+    int age, height, weight;
+
+    EditText nameInput;
+    EditText ageInput;
+    EditText heightIntput;
+    EditText weightInput;
+    EditText mealInput;
+
+    Button submitButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.enter_info);
+
+        nameInput = (EditText) findViewById(R.id.nameInput);
+        ageInput = (EditText) findViewById(R.id.ageInput);
+        heightIntput = (EditText) findViewById(R.id.heightInput);
+        weightInput = (EditText) findViewById(R.id.heightInput);
+        mealInput = (EditText) findViewById(R.id.mealInput);
+
+
+        submitButton = (Button) findViewById(R.id.button);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name = nameInput.getText().toString();
+                age = Integer.valueOf(ageInput.getText().toString());
+                height = Integer.valueOf(heightIntput.getText().toString());
+                weight = Integer.valueOf(weightInput.getText().toString());
+                meal = mealInput.getText().toString();
+
+            }
+        });
+
+
+
+    }
+}
