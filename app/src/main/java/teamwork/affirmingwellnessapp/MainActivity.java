0fbfad4d8773cc6c87275c7cd3d11354878535e2
Angela Dai <zhjh.angela@gmail.com>
2020-10-08 22:49:19 -0400

    Connecting to the API

--- app/src/main/java/teamwork/affirmingwellnessapp/MainActivity.java
+++ app/src/main/java/teamwork/affirmingwellnessapp/MainActivity.java
@@ -1,8 +1,23 @@
 package teamwork.affirmingwellnessapp;
 
+<<<<<<< Updated upstream
+=======
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+>>>>>>> Stashed changes
 import androidx.appcompat.app.AppCompatActivity;
 
+<<<<<<< Updated upstream
 import android.os.Bundle;
+=======
+public class MainActivity extends AppCompatActivity {
+    private Button summaryButton;
     private Button settingsButton;
     private Button inputButton;
     private Button resourceButton;
